ccc7f4949b572a9ec1221a76e3155764ec73c59a
Artur <lozinski.artur@gmail.com>
2023-08-15 14:07:44 +0200

    optimisation multiply by n and divide by n

--- regresja.m
+++ regresja.m
@@ -8,12 +8,12 @@ function regr = regresja( v, n, delta = 1 )
 
 	regr = zeros(lenv-n+1,1);
 
-	DELTA = 12/(delta*n*n*(n*n-1));
+	DELTA = 12/(delta*n*(n*n-1));
 	sy = sum(v(1:n));
 	sxy = sum(v(1:n).*[1:n])
 
 	for l = 1:length(regr)
-		regr(l) = DELTA*n*(sxy-(n+1)/2*sy);
+		regr(l) = DELTA*(sxy-(n+1)/2*sy);
 		if( l != length(regr) )
 			sxy=sxy-sy+n*v(n+l);
 			sy=sy-v(l)+v(n+l);
