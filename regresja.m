e861b4f17632a26e65e73f3fab91b7ad00511670
Artur <lozinski.artur@gmail.com>
2023-08-15 13:39:34 +0200

    init octave script

---
+++ regresja.m
@@ -0,0 +1,23 @@
+function regr = regresja( v, n, delta = 1 )
+
+	lenv = length(v);
+	if( lenv < n )
+		regr = [NaN];
+		return;
+	endif
+
+	regr = zeros(lenv-n+1,1);
+
+	DELTA = 12/(delta*n*n*(n*n-1));
+	sy = sum(v(1:n));
+	sxy = sum(v(1:n).*[1:n])
+
+	for l = 1:length(regr)
+		regr(l) = DELTA*n*(sxy-(n+1)/2*sy);
+		if( l != length(regr) )
+			sxy=sxy-sy+n*v(n+l);
+			sy=sy-v(l)+v(n+l);
+		endif
+	endfor
+
+endfunction
