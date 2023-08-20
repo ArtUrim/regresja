b3176bcc135827ae499975dbf362b315b025cf18
Artur <lozinski.artur@gmail.com>
2023-08-20 20:56:05 +0200

    read data

--- ctest/gt.cpp
+++ ctest/gt.cpp
@@ -5,16 +5,16 @@ extern  "C" {
 }
 
 // The fixture for testing class Foo.
-class RegresjaTest : public ::testing::Test {
+class RegresjaBasic : public ::testing::Test {
  protected:
   // You can remove any or all of the following functions if their bodies would
   // be empty.
 
-  RegresjaTest() {
+  RegresjaBasic() {
      // You can do set-up work for each test here.
   }
 
-  ~RegresjaTest() override {
+  ~RegresjaBasic() override {
      // You can do clean-up work that doesn't throw exceptions here.
   }
 
@@ -38,8 +38,7 @@ class RegresjaTest : public ::testing::Test {
 #define N 10
 #define STEPS 5
 
-// Tests that the Foo::Bar() method does Abc.
-TEST_F(RegresjaTest, Basic) {
+TEST_F(RegresjaBasic, o5) {
 
 	float buffer[STEPS];
 	regresja_init( STEPS, buffer, 0.1f ); 
@@ -53,6 +52,29 @@ TEST_F(RegresjaTest, Basic) {
 			EXPECT_EQ( regresja_ready(), true );
 			EXPECT_FLOAT_EQ( val, 10.0f );
 		}
+	}
+  
+}
+
+TEST_F(RegresjaBasic, o10) {
+
+	float buffer[STEPS];
+	EXPECT_EQ( regresja_init( STEPS, buffer, 1.0f ), 0 );
+	for( int i = 0; i < N; i++ )
+	{
+		float val = regresja( (float)i );
+		if( i < 4 )
+		{
+			EXPECT_EQ( regresja_ready(), false );
+		} else {
+			EXPECT_EQ( regresja_ready(), true );
+			EXPECT_FLOAT_EQ( val, 1.0f );
+		}
+	}
+	for( int i = N; i < 15; i++ ) {
+		float val = regresja( 2.0*i );
+		if( i == 14 )
+			EXPECT_FLOAT_EQ( val, 2.0 );
 	}
   
 }
--- ctest/gtest_onecurve.cpp
+++ ctest/gtest_onecurve.cpp
@@ -1,4 +1,8 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
 
 extern  "C" {
 #include "../regresja.h"
@@ -6,12 +10,19 @@ extern  "C" {
 
 // The fixture for testing class Foo.
 class OneCurveTest : public ::testing::Test {
+	static std::vector< std::vector<float> > vv;
+	static bool initDone;
+	static void initData();
+
  protected:
   // You can remove any or all of the following functions if their bodies would
   // be empty.
 
   OneCurveTest() {
      // You can do set-up work for each test here.
+	  if( initDone == false )
+		  OneCurveTest::initData();
+	  initDone = true;
   }
 
   ~OneCurveTest() override {
@@ -34,6 +45,46 @@ class OneCurveTest : public ::testing::Test {
   // Class members declared here can be used by all tests in the test suite
   // for Foo.
 };
+
+std::vector< std::vector<float> > OneCurveTest::vv( 9, std::vector<float>(20) );
+bool OneCurveTest::initDone = false;
+void OneCurveTest::initData()
+{
+	std::ifstream ifh("../octave/onecurve.csv");
+	std::string line;
+	if( ifh.good() )
+	{
+		int lnum = 0;
+		while( std::getline( ifh, line ) ) {
+			int pos = 0;
+			std::vector<float> & rv = vv[lnum++];
+			char *pch = strtok( const_cast<char *>(line.c_str()), "," );
+			while( pch != NULL ) {
+				try {
+					rv.at(pos) =  atof( pch );
+				} catch( std::out_of_range const & e ) {
+					rv.resize( 2*rv.size() );
+					rv[pos] = atof( pch );
+				}
+				pos++;
+				pch = strtok( NULL, "," );
+			}
+			if( rv.size() > pos ) {
+				rv.resize(pos);
+			}
+		}
+	}
+	ifh.close();
+
+	for( std::vector< std::vector<float> >::iterator il = vv.begin();
+			il != vv.end(); il++ ) {
+		for( std::vector<float>::iterator ip = (*il).begin(); ip != (*il).end(); ip++ )
+		{
+			std::cout << *ip << " ";
+		}
+		std::cout << std::endl;
+	}
+}
 
 #define N 10
 #define STEPS 5
